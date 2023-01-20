41f32847318013ca57725e11c03c9043cb786eaa
vinayakgarole <vinayakgarole10@gamil.com>
2023-01-20 14:21:55 +0530

    Added: Prints the power of number 2

---
+++ src/com/bridgelabz/PowerOf2.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PowerOf2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter powers of Two: ");
+        int x = sc.nextInt();
+        int n = 2;
+        int result = 1;
+
+        for (int i = 1; i <= x; i++) {
+            result = n * result;
+        }
+        System.out.println("Power = " + result);
+    }
+}
